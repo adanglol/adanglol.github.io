41cdb23669d0dd1252d3eae02b3ef197a8d84a19
Aaron Dangc <adraging@gmail.com>
2023-05-07 23:15:07 -0700

    edit

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn Reactdfd
         </a>
       </header>
     </div>
